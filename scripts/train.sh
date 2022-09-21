cbcff67fffd68ef7b734fcb184f7c7d2f8703cb5
VanChilly <1226243380@qq.com>
2022-09-21 22:16:11 +0800

    [Add] Add a start.sh file and modify train.sh to support log

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python non-quantization/imagenet.py \
+python -u non-quantization/imagenet.py \
     --arch parallel_resnet18 \
     --data /HOME/scz0831/run/prune/dataset/cifar10 \
     --worker 2 \
     --epochs 120 \
     --sizes 224 192 160 128 96 \
     --kd \
-    --kd-type ens_topdown
+    --kd-type ens_topdown > log/log_test_train.log
