b094f1060cae937934ef21da1b9d21192344aa6c
VanChilly <1226243380@qq.com>
2022-09-23 12:10:11 +0800

    [Update] Update train.sh to support ens-top-down kd

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# for ens-topdown kd
+# python -u non-quantization/imagenet.py \
+#     --arch parallel_resnet18 \
+#     --data /HOME/scz0831/run/prune/dataset/cifar10 \
+#     --worker 2 \
+#     --epochs 120 \
+#     --sizes 224 192 160 128 96 \
+#     --kd \
+#     --kd-type ens > log/log.log
+
 # for ens kd
 # python -u non-quantization/imagenet.py \
 #     --arch parallel_resnet18 \
